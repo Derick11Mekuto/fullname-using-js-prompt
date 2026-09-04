3fa10a75fc8b2a0ceee1124cc96385354778bb0e
Derick11Mekuto <bonaniderickmekuto@gmail.com>
2026-09-04 15:20:04 +0200

    Add files via upload

---
+++ fullName.js
@@ -0,0 +1,12 @@
+//prompt enter name
+var name = prompt("Enter name:");
+
+//prompt the user to enter surname
+var surname = prompt("Enter surname:");
+
+//convert the input
+var fullName = name + " " + surname ;
+
+//display them 
+document.write("Your full name is: " + fullName);
+    
